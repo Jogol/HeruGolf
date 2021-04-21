1e77a08343a54eabd827307801e67b62ef73d232
Jonathan Golan <Jonathanbgolan@gmail.com>
2021-04-21 23:38:20 +0200

    Progress

--- src/com/company/HeruGolfGenerator.java
+++ src/com/company/HeruGolfGenerator.java
@@ -1,4 +1,62 @@
 package com.company;
 
+import java.util.Random;
+
 public class HeruGolfGenerator {
+
+    int width;
+    int height;
+    float hazardRatio = 0.1f;
+    float fillRatio = 0.7f;
+    int[][] boardState;
+
+    HeruGolfGenerator(int width, int height) {
+        this.width = width;
+        this.height = height;
+
+        boardState = new int[width][height];
+
+        generateHazards();
+        printBoardState();
+    }
+
+    private void printBoardState() {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                System.out.print(boardState[x][y] + " ");
+            }
+            System.out.println();
+        }
+    }
+    private void generateHazards() {
+        int hazardNumber = Math.round(width * height * hazardRatio);
+
+        Random rand = new Random();
+        for (int i = 0; i < hazardNumber; i++) {
+            boardState[rand.nextInt(width)][rand.nextInt(height)] = TileState.HAZARD.getValue();
+        }
+    }
+
+    private double getGaussian(double aMean, double aVariance){
+        Random fRandom = new Random();
+        return aMean + fRandom.nextGaussian() * aVariance;
+    }
+
+    public enum TileState {
+        HORIZONTAL(0),
+        VERTICAL(1),
+        HAZARD(2),
+        BALL(3),
+        HOLE(4);
+
+        private final int value;
+        private TileState(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //HeruGolfGenerator golfGenerator = new HeruGolfGenerator(5, 7);
+
+        int[] results = new int[7];
+
+        for (int i = 0; i < 1000; i++) {
+            double res = getGaussian(3, 1);
+            if (res > 0 && res < 1) {
+                results[0]++;
+            } else if (res > 1 && res < 2) {
+                results[1]++;
+            } else if (res > 2 && res < 3) {
+                results[2]++;
+            } else if (res > 3 && res < 4) {
+                results[3]++;
+            } else if (res > 4 && res < 5) {
+                results[4]++;
+            } else if (res > 5 && res < 6) {
+                results[5]++;
+            }
+        }
+
+        for (int result : results) {
+            System.out.print((double) result/100 + " ");
+        }
+
+    }
+
+    private static double getGaussian(double aMean, double aVariance){
+        Random fRandom = new Random();
+        return aMean + fRandom.nextGaussian() * aVariance;
     }
 }
